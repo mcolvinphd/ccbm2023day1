adffaefbff63645e18ac3c98b5185cf4830f4819
mcolvinphd <mcolvin@ucmerced.edu>
2020-07-15 11:10:44 -0700

    first commit

---
+++ rwalknd.py
@@ -0,0 +1,15 @@
+from random import choice
+dim = 2
+trials = 100
+steps = 100000
+gothome = 0
+for i in range(trials):
+    point = [0]*dim
+    for step in range(steps):
+        for j in range(dim):
+            point[j] += choice((-1, 1))
+        if point.count(0) == dim:
+            gothome += 1
+            break
+print("Fraction that got home=%f in %d dims" % (gothome/trials, dim))
+
